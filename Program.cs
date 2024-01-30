aeaed0b007b071db6e1d21c04fa78bbd27edb4f6
Rysyq <wikto-ol@wp.pl>
2024-01-30 09:43:27 +0100

    function work

--- Program.cs
+++ Program.cs
@@ -1,10 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-//przyjmowanie i porównywanie - matematycznie
-//nazwij swego gracza
-//punkty
-
-using System.Formats.Asn1;
+﻿using System.Buffers;
+using System.Runtime.InteropServices;
 
 string[] availableAnimals = { "cat", "dog", "goose", "sheep", "horse" };
 
@@ -22,6 +17,12 @@ Random ua = new Random();
 Console.WriteLine("Quess the animal!");
 Console.WriteLine();
 
+int points = 0;
+
+string coffeeanswer;
+
+string yourName = PlayersName("Steven");
+string secondOneName = PlayersName("Steve");
 
 while (true)
 {
@@ -33,27 +34,25 @@ while (true)
 
     Console.WriteLine();
 
-    string? choosedAnimal;
+    string choosedAnimal;
     Console.WriteLine("Which animal was choosed? Provide animal or write 'end' the game: ");
-    string? quessedAnimal = Console.ReadLine()?.ToLower().Trim();
+    string quessedAnimal = Console.ReadLine()?.ToLower().Trim() ?? string.Empty;
     Console.WriteLine();
-
 
     int quessedAnimalIndex = Array.IndexOf(availableAnimals, quessedAnimal);
 
     while (!availableAnimals.Contains(quessedAnimal) && quessedAnimal != "end" && quessedAnimal != "ultimate animal")
     {
         Console.WriteLine("Really? Provide animal or write 'end' to end the game.");
-        quessedAnimal = Console.ReadLine()?.ToLower().Trim();
+        quessedAnimal = Console.ReadLine()?.ToLower().Trim() ?? string.Empty;
     }
 
     int choosedanimalIndex = rng.Next(availableAnimals.Length);
     choosedAnimal = availableAnimals[choosedanimalIndex];
 
-    string? answer;
+    string answer;
 
-    string? ultimateAnimal;
-
+    string ultimateAnimal;
 
     if (quessedAnimal == "end")
     {
@@ -63,6 +62,7 @@ while (true)
     if (quessedAnimal == choosedAnimal)
     {
         Console.WriteLine($"You're right! It's the {choosedAnimal}!");
+        points++;
     }
     else if (quessedAnimal == "ultimate animal")
     {
@@ -85,13 +85,13 @@ while (true)
             string? yesOrNo;
             string? coffeeYesNo;
 
-            answer = Console.ReadLine();
+            answer = Console.ReadLine()?.ToLower().Trim() ?? string.Empty;
             Console.WriteLine();
 
             while (!availableAnswers.Contains(answer) && answer != "back")
             {
                 Console.WriteLine("Really? Why do you even try this? Write 'back' to come back!");
-                answer = Console.ReadLine()?.ToLower().Trim();
+                answer = Console.ReadLine()?.ToLower().Trim() ?? string.Empty;
                 Console.WriteLine();
 
             }
@@ -101,15 +101,16 @@ while (true)
                 Console.WriteLine($"You can see: {ultimateAnimal} ");
 
                 Console.WriteLine("Do you want to pet it? yes/no");
-                yesOrNo = Console.ReadLine()?.ToLower().Trim();
+                yesOrNo = Console.ReadLine()?.ToLower().Trim() ?? string.Empty;
                 Console.WriteLine();
 
                 while (yesOrNo != "yes" && yesOrNo != "no")
                 {
                     Console.WriteLine("Really? Why do you even try this?");
-                    yesOrNo = Console.ReadLine()?.ToLower().Trim();
+                    yesOrNo = Console.ReadLine()?.ToLower().Trim() ?? string.Empty;
                     Console.WriteLine();
                 }
+
                 if (yesOrNo == "yes")
                 {
                     Console.WriteLine($"The {ultimateAnimal} is happy!");
@@ -124,24 +125,23 @@ while (true)
             }
             else if (answer == "history")
             {
-                Console.WriteLine("My name is Steve. I don't have idea who I am and who you are. It kinda weird, isn't it? All the story, nothing more.");
+                Console.WriteLine($"My name is {secondOneName}. I don't have idea who I am and who you are. It kinda weird, isn't it? All the story, nothing more.");
                 Console.WriteLine();
             }
             else if (answer == "coffee")
             {
                 Console.WriteLine("Wanna coffee? why not/not today");
-                coffeeYesNo = Console.ReadLine()?.ToLower().Trim();
+                coffeeYesNo = Console.ReadLine()?.ToLower().Trim() ?? string.Empty;
                 Console.WriteLine();
 
                 while (coffeeYesNo != "why not" && coffeeYesNo != "not today")
                 {
                     Console.WriteLine("Really? Why do you even try this?");
-                    coffeeYesNo = Console.ReadLine()?.ToLower().Trim();
+                    coffeeYesNo = Console.ReadLine()?.ToLower().Trim() ?? string.Empty;
                     Console.WriteLine();
                 }
                 if (coffeeYesNo == "why not")
                 {
-                    string? coffeeanswer;
                     Console.WriteLine("Menu: ");
                     foreach (string coffeeType in menu)
                     {
@@ -149,7 +149,15 @@ while (true)
                     }
                     Console.WriteLine();
                     Console.WriteLine("Which coffee do you want?");
-                    coffeeanswer = Console.ReadLine();
+                    coffeeanswer = Console.ReadLine()?.ToLower().Trim() ?? string.Empty;
+
+                    while (!menu.Contains(coffeeanswer))
+                    {
+                        Console.WriteLine("Really? Why do you even try this?");
+                        coffeeanswer = Console.ReadLine()?.ToLower().Trim() ?? string.Empty;
+                        Console.WriteLine();
+                    }
+
                     Console.WriteLine();
                     Console.WriteLine($"That's your {coffeeanswer}!");
                     Console.WriteLine();
@@ -167,9 +175,6 @@ while (true)
             }
         }
 
-        //int answerIndex = Array.IndexOf(availableAnswers, answer);
-        //int otherAnswerIndex = (answerIndex + 1) % availableAnswers.Length;
-        //string otherAnswer = availableAnswers[otherAnswerIndex];
     }
     else
     {
@@ -187,7 +192,30 @@ while (true)
             Console.WriteLine();
         }
     }
+
+    Console.WriteLine($"{yourName}, you have points: {points}");
+
+    if (points >= 5)
+    {
+        Console.WriteLine("You have unlocked command: ultimate animal");
+        Console.WriteLine();
+    }
+
 }
 
 Console.WriteLine("Clik anything to end the game...");
 Console.ReadLine();
+
+string PlayersName(string name)
+{
+    Console.WriteLine($"Provide name, {name}: ");
+    string naming = Console.ReadLine()?.Trim() ?? string.Empty;
+    if (string.IsNullOrWhiteSpace(naming))
+    {
+        return $"Name: {name}";
+    }
+    else
+    {
+        return naming;
+    }
+}
